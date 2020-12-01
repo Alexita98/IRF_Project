919d28f0d1625ad7db0230f126af97cd8394b23d
Alexandra Szundy <alexandra.szundy@stud.uni-corvinus.hu>
2020-12-01 10:55:14 +0100

    Filmválasztáshoz ComboBox és Linq

--- Cinema_booking_RPCYYH/Cinema_booking_RPCYYH/Form1.Designer.cs
+++ Cinema_booking_RPCYYH/Cinema_booking_RPCYYH/Form1.Designer.cs
@@ -31,19 +31,18 @@ namespace Cinema_booking_RPCYYH
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.label5 = new System.Windows.Forms.Label();
+            this.labelTitel = new System.Windows.Forms.Label();
+            this.cboxTime = new System.Windows.Forms.ComboBox();
+            this.labelSeatNumber = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cboxMovie = new System.Windows.Forms.ComboBox();
             this.btnSeats = new System.Windows.Forms.Button();
-            this.comboBox5 = new System.Windows.Forms.ComboBox();
-            this.comboBox4 = new System.Windows.Forms.ComboBox();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.txtboxTitle = new System.Windows.Forms.TextBox();
+            this.cboxYear = new System.Windows.Forms.ComboBox();
             this.label6 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.labelTitel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -60,43 +59,53 @@ namespace Cinema_booking_RPCYYH
             // 
             this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.splitContainer1.Panel1.Controls.Add(this.labelTitel);
-            this.splitContainer1.Panel1.Controls.Add(this.comboBox2);
-            this.splitContainer1.Panel1.Controls.Add(this.label5);
+            this.splitContainer1.Panel1.Controls.Add(this.cboxTime);
+            this.splitContainer1.Panel1.Controls.Add(this.labelSeatNumber);
             this.splitContainer1.Panel1.Controls.Add(this.label4);
             this.splitContainer1.Panel1.Controls.Add(this.label3);
             this.splitContainer1.Panel1.Controls.Add(this.label2);
-            this.splitContainer1.Panel1.Controls.Add(this.comboBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.cboxMovie);
             this.splitContainer1.Panel1.Controls.Add(this.btnSeats);
             // 
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.BackColor = System.Drawing.SystemColors.Control;
-            this.splitContainer1.Panel2.Controls.Add(this.comboBox5);
-            this.splitContainer1.Panel2.Controls.Add(this.comboBox4);
-            this.splitContainer1.Panel2.Controls.Add(this.comboBox3);
+            this.splitContainer1.Panel2.Controls.Add(this.txtboxTitle);
+            this.splitContainer1.Panel2.Controls.Add(this.cboxYear);
             this.splitContainer1.Panel2.Controls.Add(this.label6);
             this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
             this.splitContainer1.Size = new System.Drawing.Size(909, 447);
             this.splitContainer1.SplitterDistance = 294;
             this.splitContainer1.TabIndex = 0;
             // 
-            // comboBox2
+            // labelTitel
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(31, 212);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(234, 28);
-            this.comboBox2.TabIndex = 10;
+            this.labelTitel.AutoSize = true;
+            this.labelTitel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelTitel.ForeColor = System.Drawing.Color.Maroon;
+            this.labelTitel.Location = new System.Drawing.Point(3, 13);
+            this.labelTitel.Name = "labelTitel";
+            this.labelTitel.Size = new System.Drawing.Size(288, 29);
+            this.labelTitel.TabIndex = 15;
+            this.labelTitel.Text = "BlahaMozi Jegyfoglalás";
             // 
-            // label5
+            // cboxTime
             // 
-            this.label5.AutoSize = true;
-            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label5.Location = new System.Drawing.Point(181, 278);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(51, 20);
-            this.label5.TabIndex = 14;
-            this.label5.Text = "label5";
+            this.cboxTime.FormattingEnabled = true;
+            this.cboxTime.Location = new System.Drawing.Point(31, 212);
+            this.cboxTime.Name = "cboxTime";
+            this.cboxTime.Size = new System.Drawing.Size(234, 28);
+            this.cboxTime.TabIndex = 10;
+            this.cboxTime.SelectedIndexChanged += new System.EventHandler(this.cboxTime_SelectedIndexChanged);
+            // 
+            // labelSeatNumber
+            // 
+            this.labelSeatNumber.AutoSize = true;
+            this.labelSeatNumber.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.labelSeatNumber.Location = new System.Drawing.Point(181, 278);
+            this.labelSeatNumber.Name = "labelSeatNumber";
+            this.labelSeatNumber.Size = new System.Drawing.Size(0, 20);
+            this.labelSeatNumber.TabIndex = 14;
             // 
             // label4
             // 
@@ -128,13 +137,14 @@ namespace Cinema_booking_RPCYYH
             this.label2.TabIndex = 11;
             this.label2.Text = "Film kiválasztása:";
             // 
-            // comboBox1
+            // cboxMovie
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(31, 126);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(234, 28);
-            this.comboBox1.TabIndex = 9;
+            this.cboxMovie.FormattingEnabled = true;
+            this.cboxMovie.Location = new System.Drawing.Point(31, 126);
+            this.cboxMovie.Name = "cboxMovie";
+            this.cboxMovie.Size = new System.Drawing.Size(234, 28);
+            this.cboxMovie.TabIndex = 9;
+            this.cboxMovie.SelectedIndexChanged += new System.EventHandler(this.cboxMovie_SelectedIndexChanged);
             // 
             // btnSeats
             // 
@@ -146,32 +156,22 @@ namespace Cinema_booking_RPCYYH
             this.btnSeats.Text = "Hely Foglalás";
             this.btnSeats.UseVisualStyleBackColor = false;
             // 
-            // comboBox5
+            // txtboxTitle
             // 
-            this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Location = new System.Drawing.Point(396, 56);
-            this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(158, 28);
-            this.comboBox5.TabIndex = 14;
-            this.comboBox5.Text = "Időpont";
+            this.txtboxTitle.Location = new System.Drawing.Point(233, 58);
+            this.txtboxTitle.Name = "txtboxTitle";
+            this.txtboxTitle.Size = new System.Drawing.Size(321, 26);
+            this.txtboxTitle.TabIndex = 14;
+            this.txtboxTitle.Text = "Title";
             // 
-            // comboBox4
+            // cboxYear
             // 
-            this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(225, 56);
-            this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(165, 28);
-            this.comboBox4.TabIndex = 13;
-            this.comboBox4.Text = "Nap";
-            // 
-            // comboBox3
-            // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(61, 56);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(158, 28);
-            this.comboBox3.TabIndex = 11;
-            this.comboBox3.Text = "Megjelenési év";
+            this.cboxYear.FormattingEnabled = true;
+            this.cboxYear.Location = new System.Drawing.Point(61, 56);
+            this.cboxYear.Name = "cboxYear";
+            this.cboxYear.Size = new System.Drawing.Size(158, 28);
+            this.cboxYear.TabIndex = 11;
+            this.cboxYear.Text = "Megjelenési év";
             // 
             // label6
             // 
@@ -185,23 +185,12 @@ namespace Cinema_booking_RPCYYH
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(61, 124);
+            this.dataGridView1.Location = new System.Drawing.Point(61, 106);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(493, 289);
+            this.dataGridView1.Size = new System.Drawing.Size(493, 307);
             this.dataGridView1.TabIndex = 10;
-            // 
-            // labelTitel
-            // 
-            this.labelTitel.AutoSize = true;
-            this.labelTitel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.labelTitel.ForeColor = System.Drawing.Color.Maroon;
-            this.labelTitel.Location = new System.Drawing.Point(3, 13);
-            this.labelTitel.Name = "labelTitel";
-            this.labelTitel.Size = new System.Drawing.Size(288, 29);
-            this.labelTitel.TabIndex = 15;
-            this.labelTitel.Text = "BlahaMozi Jegyfoglalás";
             // 
             // Form1
             // 
@@ -230,18 +219,17 @@ namespace Cinema_booking_RPCYYH
 
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.Button btnSeats;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cboxTime;
+        private System.Windows.Forms.ComboBox cboxMovie;
+        private System.Windows.Forms.Label labelSeatNumber;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBox5;
-        private System.Windows.Forms.ComboBox comboBox4;
-        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox cboxYear;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label labelTitel;
+        private System.Windows.Forms.TextBox txtboxTitle;
     }
 }
 
--- Cinema_booking_RPCYYH/Cinema_booking_RPCYYH/Form1.cs
+++ Cinema_booking_RPCYYH/Cinema_booking_RPCYYH/Form1.cs
@@ -16,11 +16,67 @@ namespace Cinema_booking_RPCYYH
         public Form1()
         {
             InitializeComponent();
-           /* this.TopMost = true;
-            this.WindowState = FormWindowState.Maximized;*/
+            /* this.TopMost = true;
+             this.WindowState = FormWindowState.Maximized;*/
 
-            
+            cboxMovie.DisplayMember = "MovieName";
+            cboxTime.DisplayMember = "StartTime";
+            //cboxYear.DisplayMember = "PublishYear";
+            //cboxDay.DisplayMember = "StartTime";
+            //cboxTime.ValueMember = "Movie_ID";
+
+            cboxMovie.DataSource = (from x in context.Movies
+                                    select x).ToList();
+            cboxYear.DataSource = (from x in context.Movies
+                                   select x.PublishYear).ToList();
+
+            // this.searchMovie();
+
+            dataGridView1.DataSource = (from x in context.Movie_Show
+                                        where x.Movie_FK == x.Movie.Movie_ID
+                                        select new
+                                        {
+                                            Film = x.Movie.MovieName,
+                                            Vetítés = x.StartTime,
+                                            Megjelenés = x.Movie.PublishYear,
+                                            Filmhossz = x.Movie.DurationMinutes
+                                        }).ToList();
+            //this.dataGridView1.Sort(this.dataGridView1.Columns["Vetítés"], ListSortDirection.Ascending);
         }
+
+        private void cboxMovie_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Movie selectedMovie = (Movie)cboxMovie.SelectedItem;
+            cboxTime.DataSource = (from x in context.Movie_Show
+                                    where x.Movie_FK == selectedMovie.Movie_ID
+                                    select x.StartTime).ToList();
+        }
+
+        private void cboxTime_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Movie_Show selectedTime = (Movie_Show)cboxTime.SelectedItem;
+            DateTime selectedTime = (DateTime)cboxTime.SelectedItem;
+            Movie selectedMovie = (Movie)cboxMovie.SelectedItem;
+            int? countFreeSeats = 50-(from x in context.Tickets
+                                   where x.Movie_Show.Movie.Movie_ID == selectedMovie.Movie_ID 
+                                   //&& x.Movie_Show_FK == selectedTime.Movie_Show_ID
+                                   && x.Movie_Show_FK == x.Movie_Show.Movie_Show_ID
+                                   && x.Movie_Show.StartTime == selectedTime
+                                   select x.Seat_FK).Count();
+            labelSeatNumber.Text = countFreeSeats.ToString();
+        }
+
+
+        /*private void searchMovie()
+        {
+            string searchValue = txtboxTitle.Text.ToLower();
+            txtboxTitle.DataSource =
+                (
+                    from t in context.Textbook //.Local szűrés optimalizálás
+                        where t.Title.ToLower().Contains(txtboxTitle.Text)
+                    select t
+               ).ToList();
+        }*/
 
     }
 }
