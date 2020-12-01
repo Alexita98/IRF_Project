104880fddc4cf8f0100d70cb481c94082777207f
Alexandra Szundy <alexandra.szundy@stud.uni-corvinus.hu>
2020-12-01 15:26:45 +0100

    Székek elrendezése

--- Cinema_booking_RPCYYH/Cinema_booking_RPCYYH/SeatBooking.Designer.cs
+++ Cinema_booking_RPCYYH/Cinema_booking_RPCYYH/SeatBooking.Designer.cs
@@ -43,7 +43,7 @@ namespace Cinema_booking_RPCYYH
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1005, 567);
+            this.ClientSize = new System.Drawing.Size(832, 567);
             this.Controls.Add(this.panel1);
             this.Name = "SeatBooking";
             this.Text = "SeatBooking";
--- Cinema_booking_RPCYYH/Cinema_booking_RPCYYH/SeatBooking.cs
+++ Cinema_booking_RPCYYH/Cinema_booking_RPCYYH/SeatBooking.cs
@@ -15,6 +15,44 @@ namespace Cinema_booking_RPCYYH
         public SeatBooking(int selectedShowID, string selectedMovieName, DateTime selectedShowTime)
         {
             InitializeComponent();
+            createCinemaHall();
+        }
+
+       // int lineWidth = 3;
+        private void createCinemaHall()
+        {
+            int row, col;
+            
+            for (row = 3; row < 7; row++)
+            {
+                for (col = 3; col < 8; col++)
+                {
+                    CinemaHall ch = new CinemaHall();
+                    //ch.Top = row * ch.Height + (int)(Math.Floor((double)(row / 3))) * lineWidth;
+                    //ch.Left = col * ch.Width + (int)(Math.Floor((double)(col / 3))) * lineWidth;
+                    ch.Top = row * ch.Height;
+                    ch.Left = col * ch.Width;
+                    panel1.Controls.Add(ch);
+                }
+            }
+            for (row = 2; row < 7; row++)
+            {
+                for (col = 10; col < 15; col++)
+                {
+                    CinemaHall ch = new CinemaHall();
+                    //ch.Top = row * ch.Height + (int)(Math.Floor((double)(row / 1))) * lineWidth;
+                    ch.Top = row * ch.Height;
+                    ch.Left = col * ch.Width;
+                    panel1.Controls.Add(ch);
+                }
+            }
+            for(col=3; col< 15; col++)
+            {
+                CinemaHall ch = new CinemaHall();
+                ch.Top = 7 * ch.Height;
+                ch.Left = col * ch.Width;
+                panel1.Controls.Add(ch);
+            }
         }
     }
 }
